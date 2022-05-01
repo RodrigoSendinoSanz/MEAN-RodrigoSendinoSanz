1782238f2e934dbcf868ee8fac4d04e5f99ce534
RodrigoSendinoSanz <78493406+RodrigoSendinoSanz@users.noreply.github.com>
2022-05-01 20:07:43 +0200

    MEANProyecto

---
+++ servidor/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const { use } = require('./routes/producto');
+const cors = require('cors');
+//Creamos el servidor
+const app = express();
+
+//conectamos a la base de datos
+conectarDB();
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/productos', require('./routes/producto'));
+
+
+app.listen(4000, () => {
+    console.log('Servidor corriendo en el puerto 4000');
+});
